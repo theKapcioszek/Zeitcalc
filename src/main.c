5b1384802c1df79c70c2fd059ce9a4c0f6f687d0
Bartłomiej Szostak <BartGOKU@onet.pl>
2023-09-23 14:51:46 +0200

    added: layout for buttons

--- src/main.c
+++ src/main.c
@@ -3,12 +3,17 @@
 
 #define WWINDOW 1280
 #define HWINDOW 720
+//#define WWINDOW 1920
+//#define HWINDOW 1080
 
 #define WWCENTER (WWINDOW / 2)
 #define HWCENTER (HWINDOW / 2)
 
 #define WROW ((WWINDOW / 100)*40)
 #define HROW ((HWINDOW / 100)*6)
+
+#define WBUT ((WWINDOW / 100)*19)
+#define HBUT ((HWINDOW / 100)*6)
 
 struct TextRow{
 
@@ -26,6 +31,8 @@ struct TextRow NewTextRow(char *text, int posx, int posy){
   struct TextRow row;
   row.posx = posx;
   row.posy = posy;
+  row.width = WROW;
+  row.height = HROW;
   row.centerw = posx + (WROW / 2);
   row.centerh = posy + (HROW / 2);
   row.text = text;
@@ -33,9 +40,22 @@ struct TextRow NewTextRow(char *text, int posx, int posy){
   return row;
 }
 
+struct TextRow NewButton(char *text, int posx, int posy, int width, int height){
+  struct TextRow button;
+  button.posx = posx;
+  button.posy = posy;
+  button.width = width;
+  button.height = height;
+  button.centerw = posx + (width / 2);
+  button.centerh = posy + (height / 2);
+  button.text = text;
+
+  return button;
+}
+
 void DrawRow(struct TextRow row){
 
-  DrawRectangle(row.posx, row.posy, WROW, HROW, LIGHTGRAY);
+  DrawRectangle(row.posx, row.posy, row.width, row.height, LIGHTGRAY);
   DrawText(row.text, row.centerw - (MeasureText(row.text,30) / 2), row.centerh - 15, 30, BLACK);
 
   return;
@@ -53,12 +73,6 @@ int main(int argc, char **argv){
 
     ClearBackground(GRAY);
 
-    //struct TextRow trTest1 = NewTextRow("test",WWCENTER - (WROW / 2),(HWCENTER - (HROW / 2)) - (HROW + 10));
-    //DrawRow(trTest1);
-    //struct TextRow trTest2 = NewTextRow("test",WWCENTER - (WROW / 2),trTest1.posy + HROW + 10);
-    //DrawRow(trTest2);
-    //struct TextRow trTest3 = NewTextRow("test",WWCENTER - (WROW / 2),trTest2.posy + HROW + 10);
-    //DrawRow(trTest3);
     int iPosyStart = (HWCENTER - (HROW / 2) - (HROW + 10)*6);
     int iPosyLast = iPosyStart;
     for (int i = 1; i <= 10; i++){
@@ -68,6 +82,15 @@ int main(int argc, char **argv){
       iPosyLast = trTestMulti.posy;
 
     }
+
+    struct TextRow trBackButton = NewButton("<",WWCENTER - (WROW / 2) - (WBUT + 10),(iPosyLast + HROW + 20),WBUT,HBUT);
+    DrawRow(trBackButton);
+    struct TextRow trForwardButton = NewButton(">",WWCENTER - (WROW / 2),(iPosyLast + HROW + 20),WBUT,HBUT);
+    DrawRow(trForwardButton);
+    struct TextRow trPageResult = NewButton("Suma: ",WWCENTER - (WROW / 2) + (WBUT + 25),(iPosyLast + HROW + 20),WBUT,HBUT);
+    DrawRow(trPageResult);
+    struct TextRow trAllResult = NewButton("Cala Suma: ",WWCENTER - (WROW / 2) + ((WBUT * 2)+35),(iPosyLast + HROW + 20),WBUT,HBUT);
+    DrawRow(trAllResult);
 
     EndDrawing();
 
