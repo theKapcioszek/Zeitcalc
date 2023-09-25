7e9cb7d9781bdc613798e2ff762e55e5036b751e
Bartłomiej Szostak <BartGOKU@onet.pl>
2023-09-25 19:41:40 +0200

    added: concating date with the text in text row

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <raylib.h>
 
 #define WWINDOW 1280
@@ -15,6 +17,8 @@
 #define WBUT ((WWINDOW / 100)*19)
 #define HBUT ((HWINDOW / 100)*6)
 
+#define MAX_STRING_LENGTH 10
+
 struct TextRow{
 
   int posx;
@@ -24,10 +28,11 @@ struct TextRow{
   int centerw;
   int centerh;
   char *text;
+  char *date;
 
 };
 
-struct TextRow NewTextRow(char *text, int posx, int posy){
+struct TextRow NewTextRow(char *date,char *text, int posx, int posy){
   struct TextRow row;
   row.posx = posx;
   row.posy = posy;
@@ -36,6 +41,7 @@ struct TextRow NewTextRow(char *text, int posx, int posy){
   row.centerw = posx + (WROW / 2);
   row.centerh = posy + (HROW / 2);
   row.text = text;
+  row.date = date;
 
   return row;
 }
@@ -49,14 +55,20 @@ struct TextRow NewButton(char *text, int posx, int posy, int width, int height){
   button.centerw = posx + (width / 2);
   button.centerh = posy + (height / 2);
   button.text = text;
+  button.date = NULL;
 
   return button;
 }
 
-void DrawRow(struct TextRow row){
+void DrawRow(struct TextRow row, char* text,int fontSize){
 
   DrawRectangle(row.posx, row.posy, row.width, row.height, LIGHTGRAY);
-  DrawText(row.text, row.centerw - (MeasureText(row.text,30) / 2), row.centerh - 15, 30, BLACK);
+  if(row.width != WROW){
+    DrawText(row.text, row.centerw - (MeasureText(row.text,fontSize) / 2), row.centerh - (fontSize / 2), fontSize, BLACK);
+  }
+  else{
+    DrawText(text, row.centerw - (MeasureText(text,fontSize) / 2), row.centerh - (fontSize / 2), fontSize, BLACK);
+  }
 
   return;
 }
@@ -66,32 +78,59 @@ int main(int argc, char **argv){
 
   InitWindow(WWINDOW,HWINDOW,"Januszex");
   SetTargetFPS(60);
+  char chpTextBuffer[MAX_STRING_LENGTH + 1] = "\0";
+  int iLetterCount = 0;
+
 
   while(!WindowShouldClose()){
 
     BeginDrawing();
 
-    ClearBackground(GRAY);
+    ClearBackground(DARKBLUE);
 
     int iPosyStart = (HWCENTER - (HROW / 2) - (HROW + 10)*6);
     int iPosyLast = iPosyStart;
     for (int i = 1; i <= 10; i++){
 
-      struct TextRow trTestMulti = NewTextRow("test",WWCENTER - (WROW / 2),(iPosyLast + HROW + 10));
-      DrawRow(trTestMulti);
+      struct TextRow trTestMulti = NewTextRow("25.09.2023","test",WWCENTER - (WROW / 2),(iPosyLast + HROW + 10));
+
+      char *chpCombinedText;
+      chpCombinedText = malloc(strlen(trTestMulti.date)+2+strlen(trTestMulti.text)+1);
+      strcpy(chpCombinedText,trTestMulti.date);
+      strcat(chpCombinedText,": ");
+      strcat(chpCombinedText,trTestMulti.text);
+
+      DrawRow(trTestMulti,chpCombinedText,30);
       iPosyLast = trTestMulti.posy;
+
+      free(chpCombinedText);
 
     }
 
     struct TextRow trBackButton = NewButton("<",WWCENTER - (WROW / 2) - (WBUT + 10),(iPosyLast + HROW + 20),WBUT,HBUT);
-    DrawRow(trBackButton);
+    DrawRow(trBackButton,NULL,40);
     struct TextRow trForwardButton = NewButton(">",WWCENTER - (WROW / 2),(iPosyLast + HROW + 20),WBUT,HBUT);
-    DrawRow(trForwardButton);
+    DrawRow(trForwardButton,NULL,40);
     struct TextRow trPageResult = NewButton("Suma: ",WWCENTER - (WROW / 2) + (WBUT + 25),(iPosyLast + HROW + 20),WBUT,HBUT);
-    DrawRow(trPageResult);
+    DrawRow(trPageResult,NULL,20);
     struct TextRow trAllResult = NewButton("Cala Suma: ",WWCENTER - (WROW / 2) + ((WBUT * 2)+35),(iPosyLast + HROW + 20),WBUT,HBUT);
-    DrawRow(trAllResult);
+    DrawRow(trAllResult,NULL,20);
     DrawText("0/0", WWCENTER - (MeasureText("0/0",20) / 2), iPosyLast + HROW + 20 + HBUT + 10, 20, BLACK);
+
+    int iPressedKey = GetCharPressed();
+
+    while(iPressedKey > 0){
+      if((iPressedKey >=32) && (iPressedKey <= 125) && (iLetterCount <= MAX_STRING_LENGTH)){
+
+        chpTextBuffer[iLetterCount] = (char)iPressedKey;
+        chpTextBuffer[iLetterCount+1] = '\0';
+        iLetterCount++;
+
+      }
+      iPressedKey = GetCharPressed();
+    }
+
+    DrawText(chpTextBuffer, WWCENTER - (MeasureText(chpTextBuffer,30) / 2), iPosyStart,30,BLACK);
 
     EndDrawing();
 
