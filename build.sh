565bed83de7585fb9390c6c210395cad223f8fe9
Bartłomiej Szostak <BartGOKU@onet.pl>
2023-10-07 20:11:16 +0200

    added raylib sourcecode and modified build.sh for more 'qol' compiling

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-gcc -lraylib ./src/main.c -o ./build/januszex
+cd ./raylib/raylib-4.5.0/src
+make 
+cd ../../../
+clang -I./raylib/raylib-4.5.0/src/ -L./raylib/raylib-4.5.0/src/ -l:libraylib.so -o build/januszex src/main.c -v
