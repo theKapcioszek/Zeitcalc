6838b5244dd01ab34ca40833d72a348393514265
Bartłomiej Szostak <BartGOKU@onet.pl>
2023-10-08 12:45:18 +0200

    working on building the project...

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 cd ./raylib/raylib-4.5.0/src
-make 
+clang -ggdb -DPLATFORM_DESKTOP -I./external/glfw/include/ -c rcore.c -o  ../build/rcore.o
+clang -ggdb -DPLATFORM_DESKTOP -I./external/glfw/include/ -c raudio.c -o  ../build/raudio.o
+clang -ggdb -DPLATFORM_DESKTOP -I./external/glfw/include/ -c rshapes.c -o  ../build/rshapes.o
+clang -ggdb -DPLATFORM_DESKTOP -I./external/glfw/include/ -c rtextures.c -o  ../build/rtextures.o
+clang -ggdb -DPLATFORM_DESKTOP -I./external/glfw/include/ -c rmodels.c -o  ../build/rmodels.o
+clang -ggdb -DPLATFORM_DESKTOP -I./external/glfw/include/ -c utils.c -o  ../build/utils.o
+clang -ggdb -DPLATFORM_DESKTOP -I./external/glfw/include/ -c rglfw.c -o  ../build/rglfw.o
+clang -ggdb -DPLATFORM_DESKTOP -I./external/glfw/include/ -c rtext.c -o  ../build/rtext.o 
+cd ../../build/
+ar -crs libraylib.a rcore.o raudio.o rshapes.o rtextures.o rmodels.o utils.o rglfw.o rtext.o
 cd ../../../
-clang -I./raylib/raylib-4.5.0/src/ -L./raylib/raylib-4.5.0/src/ -l:libraylib.so -o build/zeitcalc src/main.c -v
+clang -I./raylib/raylib-4.5.0/src/ -L./raylib/raylib-4.5.0/build/ -l:libraylib.a -o build/zeitcalc src/main.c -v
